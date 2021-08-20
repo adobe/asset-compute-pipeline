39b7bc269e0fdf904a163aa95cf4258c6101f5a0
Bennie Boone <bboone@adobe.com>
2021-08-20 13:47:30 -0700

    added test cases for metadata modula

--- test/metadata.test.js
+++ test/metadata.test.js
@@ -21,51 +21,93 @@ const { getImageMetadata } = require('../lib/metadata');
 const child_process = require('child_process');
 
 // Note: You need exiftool installed locally to run these tests
-describe('getMetadata (not mocked)', function() {
-    it("gets orientation metadata from a local file (jpeg) ", async function(){
-        const metadata = await getImageMetadata('./test/files/landscape8.jpg');
+describe('getMetadata from local file', function() {
+    const IMAGE_WITH_ORIENTATION = 'https://raw.githubusercontent.com/adobe/asset-compute-pipeline/NUI-1460/fix/test/files/landscape8.jpg';
+    const IMAGE_WITHOUT_ORIENTATION = 'https://raw.githubusercontent.com/adobe/asset-compute-pipeline/NUI-1460/fix/test/files/red_dot_alpha0.5.png';
+    const LOCAL_WITH_ORIENTATION = './test/files/landscape8.jpg';
+    const LOCAL_WITHOUT_ORIENTATION = './test/files/red_dot_alpha0.5.png';
+    const IMAGE_DOWNLOAD_TIMEOUT = 5000;
+
+    it("gets orientation metadata from a local file w/ orientation (jpeg) ", async function(){
+        const metadata = await getImageMetadata(LOCAL_WITH_ORIENTATION);
         assert.strictEqual(metadata.Orientation, "Rotate 270 CW");
     });
 
-    it("gets metadata from a local file w/o orientation (png) ", async function(){
-        const metadata = await getImageMetadata('./test/files/red_dot_alpha0.5.png');
+    it("gets metadata from a local file w/o orientation (png)", async function(){
+        const metadata = await getImageMetadata(LOCAL_WITHOUT_ORIENTATION);
         assert.strictEqual(metadata.Orientation, undefined);
     });
 
-    it("gets metadata from a mocked metadata (jpeg) ", async function(){
+    it("attempt to get metadata from Image URL with orientation (jpeg) ", async function(){
+        const metadata = await getImageMetadata(IMAGE_WITH_ORIENTATION);
+        assert.strictEqual(metadata.Orientation, "Rotate 270 CW");
+    }).timeout(IMAGE_DOWNLOAD_TIMEOUT);
+
+    it("attempt to get metadata from Image URL  w/o  Orientation (jpeg) ", async function(){
+        const metadata = await getImageMetadata(IMAGE_WITHOUT_ORIENTATION);
+        assert.strictEqual(metadata.Orientation, undefined);
+    }).timeout(IMAGE_DOWNLOAD_TIMEOUT);
+
+    it("gets metadata from a local file with orientation (mocked) metadata (jpeg) ", async function(){
         child_process._original_execSync = child_process.execSync;
         child_process.execSync = function () {
             console.log('Mocked execSync');
             return JSON.stringify([{
-                "SourceFile": "./lakers-2560x1600.jpg",
-                "Orientation": "Rotate 270 CW",
-                "ExifToolVersion": 12.00,
-                "FileName": "lakers-2560x1600.jpg",
-                "Directory": "./test/test-files",
-                "FileSize": "114 kB",
-                "FileModifyDate": "2021:08:06 07:58:16-07:00",
-                "FileAccessDate": "2021:08:06 07:58:20-07:00",
-                "FileInodeChangeDate": "2021:08:06 07:58:16-07:00",
-                "FilePermissions": "rw-r--r--",
-                "FileType": "JPEG",
-                "FileTypeExtension": "jpg",
-                "MIMEType": "image/jpeg",
-                "JFIFVersion": 1.01,
-                "ResolutionUnit": "None",
-                "XResolution": 1,
-                "YResolution": 1,
-                "ImageWidth": 2560,
                 "ImageHeight": 1600,
                 "EncodingProcess": "Baseline DCT, Huffman coding",
                 "BitsPerSample": 8,
-                "ColorComponents": 3,
+                "Orientation": "Rotate 270 CW"
+            }]);
+        };
+        const metadata = await getImageMetadata(LOCAL_WITH_ORIENTATION);
+        assert.strictEqual(metadata.Orientation, "Rotate 270 CW");
+        child_process.execSync = child_process._original_execSync;
+    });
+
+    it("gets metadata from a local file w/o orientation with (mocked) metadata (jpeg) ", async function(){
+        child_process._original_execSync = child_process.execSync;
+        child_process.execSync = function () {
+            console.log('Mocked execSync');
+            return JSON.stringify([{
+                "ImageHeight": 1600,
+                "EncodingProcess": "Baseline DCT, Huffman coding"
+            }]);
+        };
+        const metadata = await getImageMetadata(LOCAL_WITHOUT_ORIENTATION);
+        assert.strictEqual(metadata.Orientation, undefined);
+        child_process.execSync = child_process._original_execSync;
+    });
+
+    it("get metadata from Image URL with (mocked) metadata with orientation (jpeg) ", async function(){
+        child_process._original_execSync = child_process.execSync;
+        child_process.execSync = function () {
+            console.log('Mocked execSync');
+            return JSON.stringify([{
+                "ImageHeight": 1600,
+                "BitsPerSample": 8,
+                "ImageSize": "2560x1600",
+                "Megapixels": 4.1,
+                "Orientation": "Rotate 270 CW"
+            }]);
+        };
+        const metadata = await getImageMetadata(IMAGE_WITH_ORIENTATION);
+        assert.strictEqual(metadata.Orientation, "Rotate 270 CW");
+        child_process.execSync = child_process._original_execSync;
+    });
+
+    it("attempt to get metadata from Image URL w/o orientation (mocked) metadata (jpeg) ", async function(){
+        child_process._original_execSync = child_process.execSync;
+        child_process.execSync = function () {
+            console.log('Mocked execSync');
+            return JSON.stringify([{
+                "ImageHeight": 1600,
                 "YCbCrSubSampling": "YCbCr4:2:0 (2 2)",
                 "ImageSize": "2560x1600",
                 "Megapixels": 4.1
             }]);
         };
-        const metadata = await getImageMetadata('./test/files/landscape8.jpg');
-        assert.strictEqual(metadata.Orientation, "Rotate 270 CW");
+        const metadata = await getImageMetadata(IMAGE_WITHOUT_ORIENTATION);
+        assert.strictEqual(metadata.Orientation, undefined);
         child_process.execSync = child_process._original_execSync;
     });
 });
