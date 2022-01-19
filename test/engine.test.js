313614702f0bcefc0f3144943aa6eb09ed2ed79e
delbick <delbick@adobe.com>
2022-01-19 11:22:27 -0800

    fix failing test

--- test/engine.test.js
+++ test/engine.test.js
@@ -887,7 +887,10 @@ describe("Pipeline Engine tests", function () {
 
         nock('https://example.com')
             .get('/fakeEarth.tiff')
-            .reply(200, "ok");
+            .reply(200, "ok", {
+                'content-type': 'image/tiff',
+                'content-length': 2
+            });
         
         const pipeline = new Engine();
         
@@ -897,6 +900,7 @@ describe("Pipeline Engine tests", function () {
         
         const input = {
             type: 'image/tiff',
+            size: 2,
             url: 'https://example.com/fakeEarth.tiff'
         };
         const output = {
@@ -914,7 +918,7 @@ describe("Pipeline Engine tests", function () {
         // assert download ran
         assert(nock.isDone());
         await pipeline.run(plan);
-    });
+    }).timeout(20000);
 
     it("Should download before refinePlan with input url (refinePlan)", async function () {
         const Engine = proxyquire('../lib/engine', 
@@ -924,7 +928,10 @@ describe("Pipeline Engine tests", function () {
 
         nock('https://example.com')
             .get('/fakeEarth.tiff')
-            .reply(200, "ok");
+            .reply(200, "ok", {
+                'content-type': 'image/tiff',
+                'content-length': 2
+            });
 
         const pipeline = new Engine();
 
@@ -934,6 +941,7 @@ describe("Pipeline Engine tests", function () {
 
         const input = {
             type: 'image/tiff',
+            size: 2,
             url: 'https://example.com/fakeEarth.tiff',
             name: "fakeEarth.tiff"
         };
